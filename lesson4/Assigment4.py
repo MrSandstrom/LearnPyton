aff798b34a7e6157a942a3c78fac1dd8821e4664
jorgen <jorgen.sandstrom@gmail.com>
2020-06-24 08:55:04 +0200

    updated lesson4

--- lesson4/Assigment4.py
+++ lesson4/Assigment4.py
@@ -3,9 +3,7 @@
 import os
 from student import Student
 
-
-
-menu_options = ["A - Add Student", "V - View Stundent(s)", "E - Edit Student","Q - Quit"]
+menu_options = ["A - Add Student", "V - View Stundent(s)", "R - Remove Student", "E - Edit Student", "Q - Quit"]
 
 students_list = []
 students_list.append(Student(first_name="Jörgen", last_name="Sandström"))
@@ -13,21 +11,21 @@ students_list.append(Student(first_name="Ludwig", last_name="Sandström"))
 
 
 def add_student():
+    print("-- Add a student --")
     first_name = input("Enter first name: ")
     last_name = input("Enter last name: ")
-
     students_list.append(Student(first_name, last_name))
-    print(f"New Student added.")
-
+    
 
 
 def list_students():
+    print("-- List all students --")
     for item in students_list:
-        print(f"Student: {item.first_name} {item.last_name}")
-    #for index, item in enumerate(students_list):
-    #    print(f"Index: {index} Name: {item.first_name} {item.last_name}")
+        print(f"Student: {item.first_name} {item.last_name}")    
+
 
 def find_student():
+    print("-- Search for a stundent --")
     search_first_name = input("Enter first name: ")
     search_last_name = input("Enter last name: ")
     selected_student = ""
@@ -35,38 +33,30 @@ def find_student():
         if student.first_name == search_first_name and student.last_name == search_last_name:
             selected_student = student
             break
-    
-    if (selected_student == ""):
+    return selected_student
+
+
+def remove_student():
+    found_student = find_student()
+    if (found_student == ""):
         print("Sorry, cannot find the person, try again...")
     else:
-        print(f"Found student: {selected_student.first_name} {selected_student.last_name}")
-    
+        print(f"Removing student: {found_student.first_name} {found_student.last_name}")
+        students_list.remove(found_student)
 
 def edit_student():
-    print("Enter some data to be able to find the student you want to edit!")
-    search_first_name = input("Enter first name: ")
-    selected_student = ""
-    for student in students_list:
-        if student.first_name == search_first_name:            
-            selected_student = student
-            break
+    found_student = find_student()
         
-    if (selected_student == ""):
+    if (found_student == ""):
         print("Sorry, cannot find the person, try again...")
     else:
-        print(f"Found student: {selected_student.first_name}")
-        updated_first_name = input("Enter the new first name: ")        
+        print(f"Found student: {found_student.first_name} {found_student.last_name}")
+        updated_first_name = input("Enter the new first name: ")
+        updated_last_name = input("Enter the new last name: ")        
         
-        students_list.remove(selected_student)
-        students_list.append(Student(first_name=updated_first_name, last_name="Jalla Jalla"))
+        students_list.remove(found_student)
+        students_list.append(Student(first_name=updated_first_name, last_name=updated_last_name))
         
-
-    
-
-
-
-
-
 
 while True:
     for item in menu_options:
@@ -81,14 +71,12 @@ while True:
         edit_student()
     elif user_input.upper() == "Q":        
         break
+    elif user_input.upper() == "R":
+        remove_student()
     elif user_input.upper() == "F":
         find_student()
     elif user_input.upper() == "C":
-        os.system('cls')
+        os.system('cls') # Experimental, think this only works in windows
     else:
         print("Not a vaild option")
-
-
-
-
 
