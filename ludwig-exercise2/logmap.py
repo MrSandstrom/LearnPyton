bf45d53852f0f4bd1f4c7d8a7afeb0312f9c1e37
jorgen <jorgen.sandstrom@gmail.com>
2023-09-04 13:52:40 +0200

    more examples

--- ludwig-exercise2/logmap.py
+++ ludwig-exercise2/logmap.py
@@ -1,7 +1,7 @@
 def logmap(r,x):    
     return (r*x*(1-x))
 
-print(logmap(float(3.9),float(0.20)))
+#print(logmap(float(3.9),float(0.20)))
 
 #--------------------------------------------------------------------
 
@@ -34,3 +34,76 @@ def table(r,x1,x2,n):
 #table(float(3.9),float(0.20),float(0.21),10)
 
 #--------------------------------------------------------------------
+#
+# Implement the function same_ends which given a list returns True/False depending on whether the first and the last elements in the list are the same. 
+# For example, same_ends(['A','B','C','D','A']) must be True. The function must return False if the list has no elements
+#
+
+
+def same_ends(array):
+    length = len(array)
+    
+    if length == 0:
+        return False
+    
+    startPos = 0
+    endPos = length -1
+
+    if array[startPos] == array[endPos]:        
+        return True
+    else:
+        return False
+
+    
+
+# array = []
+array = ['A','B','C','D','B','A']
+#print(same_ends(array))
+
+
+#--------------------------------------------------------------------
+
+#
+# All Internet pages are written in the language HTML, where plain text is intermixed with tags which encode formatting instructions. 
+# For example <b>attention</b> indicates that the page should show the text 'attention' in bold font. 
+# Similarly <i>attention</i> indicates italic style.
+#
+# Implement the function tag for adding tags to text, e.g. tag('b','hello') must return '<b>hello</b>'
+
+def tag(b, hello):
+    value = "<" + b + ">"
+    return value+hello+value
+
+#print(tag('b','hello'))
+
+
+#--------------------------------------------------------------------
+#
+# Given a list with an even number of elements, generate a new list where the first and the second halves of the original are swapped.
+# For example, swap([1,2,3,4]) must return [3,4,1,2].
+
+def swap(array):
+    half = int(len(array)/2)
+    first_half = array[0:half] 
+    second_half = array[half:len(array)]
+    result = second_half + first_half
+    return result
+
+print(swap([1,2,3,4,5,6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
